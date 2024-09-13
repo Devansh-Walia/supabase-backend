13f246fabca93e9b096180e53cd4f24da5ca0ceb
Devansh-Walia <devanshwalia9898@gmail.com>
2024-09-14 00:29:12 +0530

    Initialize Supabase client with configuration

---
+++ src/supabase/index.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+import { applicationConfig } from "../config";
+import assert from "assert";
+
+assert(applicationConfig.supabase.supabaseUrl, "Supabase URL is not defined");
+assert(applicationConfig.supabase.supabaseKey, "Supabase Key is not defined");
+
+const supabase = createClient(
+  applicationConfig.supabase.supabaseUrl,
+  applicationConfig.supabase.supabaseKey
+);
+
+export default supabase;
