d585dab2bb934b71a1c37bd836ddd6b2e248de5b
Devansh-Walia <devanshwalia9898@gmail.com>
2024-09-14 00:33:52 +0530

    Add ESLint and Prettier for code quality and formatting
    This commit introduces ESLint and Prettier to enhance code quality and
    maintain consistent formatting. It adds configuration files for both
    tools, along with new npm scripts for linting and formatting. These
    changes will help enforce coding standards and improve overall code
    maintainability across the project.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import { applicationConfig } from "./config";
+import express from 'express';
+import { applicationConfig } from './config';
 
 const app = express();
 const port = applicationConfig.port;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello! Welcome to the Supabase MFA API service");
+app.get('/', (req, res) => {
+  res.send('Hello! Welcome to the Supabase MFA API service');
 });
 
 app.listen(port, () => {
--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -1,9 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
-import { applicationConfig } from "../config";
-import assert from "assert";
+import { createClient } from '@supabase/supabase-js';
+import { applicationConfig } from '../config';
+import assert from 'assert';
 
-assert(applicationConfig.supabase.supabaseUrl, "Supabase URL is not defined");
-assert(applicationConfig.supabase.supabaseKey, "Supabase Key is not defined");
+assert(applicationConfig.supabase.supabaseUrl, 'Supabase URL is not defined');
+assert(applicationConfig.supabase.supabaseKey, 'Supabase Key is not defined');
 
 const supabase = createClient(
   applicationConfig.supabase.supabaseUrl,
